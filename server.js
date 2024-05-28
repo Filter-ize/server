ca32514d2651f707ab222eeb71d7b80994ccda3a
iKNZR <erickson.1207@gmail.com>
2024-05-28 02:36:03 -0500

    cors

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
     cors({
-        origin: ['http://localhost:5173'],
+        origin: ['http://localhost:5173', 'https://filter-ize-app.onrender.com'],
         credentials: true,
     })
 )

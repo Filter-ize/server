edd42012944512b50fa52ffa148f2aceaf829bc9
iKNZR <erickson.1207@gmail.com>
2024-05-29 17:41:03 -0500

    fixed userFindone on controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,7 +26,7 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     //Check if user exists
-    const userExists = await User.findIne({ email });
+    const userExists = await User.findOne({ email });
 
     if (userExists) {
         res.status(400);

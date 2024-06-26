57065d87ab8633510c211529346f7eca86289074
iKNZR <erickson.1207@gmail.com>
2024-06-26 01:13:49 -0500

    finished

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -2,21 +2,7 @@ const asyncHandler = require('express-async-handler');
 const Employee = require('../models/employeeModel');
 const { fileSizeFormatter } = require('../utils/fileUpload');
 const cloudinary = require('cloudinary').v2;
-const axios = require('axios');
-const { GridFsStorage } = require('multer-gridfs-storage');
-const multer = require('multer');
-const crypto = require('crypto');
-const path = require('path');
-const mongoose = require('mongoose');
-const Grid = require('gridfs-stream');
 
-let gfs;
-
-const conn = mongoose.connection;
-conn.once('open', () => {
-    gfs = Grid(conn.db, mongoose.mongo);
-    gfs.collection('pdfs');
-});
 
 cloudinary.config({
     cloud_name: 'dlsyupxsl',
@@ -24,27 +10,6 @@ cloudinary.config({
     api_secret: 'pzK--0Yt0QaaHJx6MNWO-Ga0Kyo'
 });
 
-// Crear storage engine
-const storage = new GridFsStorage({
-    url: process.env.MONGO_URI,
-    file: (req, file) => {
-        return new Promise((resolve, reject) => {
-            crypto.randomBytes(16, (err, buf) => {
-                if (err) {
-                    return reject(err);
-                }
-                const filename = buf.toString('hex') + path.extname(file.originalname);
-                const fileInfo = {
-                    filename: filename,
-                    bucketName: 'pdfs'
-                };
-                resolve(fileInfo);
-            });
-        });
-    }
-});
-
-const upload = multer({ storage });
 
 //Create a new employee
 const createEmployee = asyncHandler(async (req, res) => {
